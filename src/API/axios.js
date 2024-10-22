cda58a27f0a641992d810350e3a9cff61ad3f9b8
Your Name <you@example.com>
2024-10-22 12:53:35 +0300

    updated axios base url with the deployed url path

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const axiosInstance =axios.create({
-    baseURL:"http://localhost:5000",
+    // baseURL:"http://localhost:5000",
+    baseURL:"https://amazon-api-8yfr.onrender.com",
+
+
 });
 
 export{axiosInstance};
