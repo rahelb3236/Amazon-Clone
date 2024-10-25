b4cf2f16c483b7bba912a61534d70c6123c6ee49
Your Name <you@example.com>
2024-10-25 14:00:10 +0300

    updated axios to refer the deployed backend api

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance =axios.create({
     // baseURL:"http://localhost:5000",
-    baseURL:"https://amazon-api-8yfr.onrender.com",
+    baseURL:"https://amazon-api-deploy-vt7p.onrender.com",
 
 
 });
